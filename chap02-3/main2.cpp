0e23bf34c34247420584f396f561400a16535d91
SEONWOO KIM <newswdddddd@gmail.com>
2026-04-17 21:42:18 +0900

    코드 수정

--- chap02-3/main2.cpp
+++ chap02-3/main2.cpp
@@ -1,43 +1,32 @@
 // **********************************************
 // 제 목 : 과제2
-// 날 짜 : 2026년 3월17일
+// 날 짜 : 2026년 4월17일
 // 작성자 : 2001485 김선우
 // **********************************************
 // 소스코드 작성
-#include <opencv2/opencv.hpp>
+#include "opencv2/opencv.hpp"
 #include <iostream>
 using namespace cv;
 using namespace std;
-
 int main()
 {
-    string files[3] = { "video1.mp4", "video2.mp4", "video3.mp4" };
-    VideoCapture cap[3];
-
-    for (int i = 0; i < 3; i++) {
-        cap[i].open(files[i]);
-        if (!cap[i].isOpened()) {
-            cout << "영상 열기 실패" << endl;
-            return -1;
-        }
-    }
-
-    Mat frame;
-    namedWindow("video", WINDOW_AUTOSIZE);
-
-    while (true)
-    {
-        for (int i = 0; i < 3; i++)
-        {
-            for (int j = 0; j < 30; j++)   // 약 1초 재생
-            {
-                cap[i] >> frame;    //프레임 읽기
-                if (frame.empty()) break;   //영상 끝 확인
-                imshow("video", frame); //화면 출력
-                if (waitKey(33) >= 0) return 0; //1000mx%33=30 프레임 이므로 영상이 자연스럽게 움직임
-            }
-        }
-    }
-
-    return 0;
+	Mat img1, img2, img3;
+	img1 = imread("lenna.bmp");
+	img2 = imread("swkim_sphoto.jpg");
+	img3 = imread("bright.jpg");
+	
+	if (img1.empty() && img2.empty() && img3.empty()) {
+		cerr << "Image load failed! " << endl;
+		return -1;
+	}
+	
+	Mat img[3] = { img1, img2, img3 };
+	
+	int i = 0;
+	while (true) {	//무한반복위해while문사용
+		imshow("image", img[i]);
+		waitKey(1000);
+		i = (i + 1) % 3;	//0,1,2,0,1,2....
+	}
+	return 0;
 }
